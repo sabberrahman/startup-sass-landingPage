9a767154e81f0fab4a7899efe7ae4c5cf81162b7
sabberrahman <sabbersr1@gmail.com>
2024-10-22 11:17:52 +0600

    feat: Navbar and top sticky notification

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,22 @@
+import ArrowIcon from "@/assets/icons/arrow-w.svg";
 export const Banner = () => {
-  return null;
+  return (
+    <main className="py-4 text-center bg-[linear-gradient(to_right,#FEECFF,#FECAFF,#FEB5FF,#FFDBE7,#FFF4D6,#F3FCD7,#D2F5EE,#C3EFFF,#FBEFFF)]">
+      <div className="container">
+        <div className="flex justify-center items-center gap-x-2">
+          <h1 className="font-medium hidden md:inline-flex">
+            This page is included in a free SaaS Website Kit.
+          </h1>
+          <div className="flex gap-2 justify-center items-center">
+            <a href="#" className="font-medium underline underline-offset-4">
+              View the complete kit
+            </a>
+            <ArrowIcon />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
+
+// 🤷‍♂️ use less flex in your code
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,40 @@
+import Image from "next/image";
+import logo from "@/assets/images/logosaas.png";
+import Menu from "@/assets/icons/menu.svg";
+
 export const Navbar = () => {
-  return null;
+  return (
+    <nav className=" bg-black">
+      <div className="container">
+        <div className="flex justify-between items-center py-5 ">
+          <div className="relative">
+            <div className="absolute top-2 bottom-0 w-full blur-md bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] "></div>
+            <Image
+              src={logo}
+              alt="Logo"
+              width={40}
+              height={41}
+              className="relative"
+            />
+          </div>
+
+          <div className="border border-white/30 p-1.5 rounded-lg md:hidden">
+            {" "}
+            <Menu className="text-white" />
+          </div>
+
+        <nav className="hidden md:flex gap-6 items-center">
+          <a href="#" className="hover:text-opacity-100 text-white text-opacity-60 transition">About</a>
+          <a href="#" className="hover:text-opacity-100 text-white text-opacity-60 transition">Features</a>
+          <a href="#" className="hover:text-opacity-100 text-white text-opacity-60 transition">Updates</a>
+          <a href="#" className="hover:text-opacity-100 text-white text-opacity-60 transition">Careers</a>
+          <a href="#" className="hover:text-opacity-100 text-white text-opacity-60 transition">Blogs</a>
+          <a href="#" className="hover:text-opacity-100 text-white text-opacity-60 transition">Cutomers</a>
+          <button className='bg-white px-[15px] py-[10px] rounded-[10px] text-black'>Get for free</button>
+        </nav>
+        </div>
+
+      </div>
+    </nav>
+  );
 };
