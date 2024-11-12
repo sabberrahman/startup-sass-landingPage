5c00860aadaf9de79368b0619868bf04f70b5c59
sabberrahman <sabbersr1@gmail.com>
2024-11-12 06:41:17 +0600

    feat: call to action + sily smol footer

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,3 +1,50 @@
+import Image from "next/image";
+import stats from "@/assets/images/emojistar.png";
+import nuts from "@/assets/images/helix2.png";
+
 export const CallToAction = () => {
-  return null;
+  return (
+    <section className="bg-black text-white py-[72px] text-center overflow-hidden">
+      <div className="container">
+        <div className="mb-10">
+          <div className="relative">
+            <h1 className="text-4xl font-semibold mb-5 md:text-5xl xl:text-6xl">
+              Get instant access
+            </h1>
+            <Image
+              src={stats}
+              alt="bigGoofyaimge"
+              width={200}
+              height={200}
+              className="absolute -top-16 -left-24 hidden md:inline-flex xl:w-[263px] xl:h-[263px]"
+            />
+
+            <Image
+              src={nuts}
+              alt="bigGoofyaimge"
+              width={200}
+              height={200}
+              className="absolute hidden md:inline-flex top-0 -right-16 xl:w-[263px] xl:h-[263px]"
+            />
+          </div>
+
+          <p className="text-lg md:w-[447px] xl:w-[520px] xl:text-xl mx-auto text-white/90 tracking-tighter ">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress and motivate your efforts.
+          </p>
+        </div>
+
+        <div className="flex gap-3 flex-col w-[300px] mx-auto md:flex-row md:w-[409px] xl:w-[520px]">
+          <input
+            placeholder="name@gmail.com"
+            type="text"
+            className="rounded-[10px] px-4 py-2 xl:text-lg bg-white/20 text-white/50 text-center flex-1"
+          />
+          <button className=" rounded-[10px] px-4 py-2 bg-white text-black xl:text-lg">
+            Get access
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,24 @@
+
+import X from "@/assets/icons/x-social.svg"
+import Insta from "@/assets/icons/insta.svg"
+import Linkedin from "@/assets/icons/linkedin.svg"
+import Youtube from "@/assets/icons/youtube.svg"
+
 export const Footer = () => {
-  return null;
+  return (
+    <section className="bg-black text-white py-8">
+      <div className="container">
+
+        <div className="md:flex md:justify-between">
+        <h1 className="text-md text-center tracking-tighter text-white/70 xl:text-xl">@ 2024 Sabber tried coding . All rights Reserved</h1>
+        <div className="flex justify-center gap-2 opacity-70 mt-2 md:mt-0 md:mr-2 ">
+           <X className="w-8 h-6 l"/>
+           <Insta className="w-8 h-6 "/>
+           <Linkedin className="w-8 h-6 "/>
+           <Youtube className="w-8 h-6 "/>
+        </div>
+        </div>
+      </div>
+    </section>
+  );
 };
